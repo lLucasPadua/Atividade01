9fe3245c4117403a8d01ea3bf29189b31db19591
unknown <lucas-padua@hotmail.com>
2021-08-10 19:51:09 -0300

    Exercício resolvido

---
+++ esconde_esconde.js
@@ -0,0 +1,10 @@
+function solucao(numero) {
+	//seu codigo aqui
+    if (numero <= 20){
+        console.log ("VOCE CONSEGUE, MANINHO")
+    } else {
+        for (let i = 21; i <= numero; i++){
+            console.log (i)
+        }
+    }
+}
